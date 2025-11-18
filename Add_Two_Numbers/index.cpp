745d25b050c0bf89aed5c7283fb74e5a3de7e8b4
victor gomes <victorgomesdacosta18@gmail.com>
2025-11-17 21:12:09 -0300

    feat: add c++ version

---
+++ Add_Two_Numbers/index.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(): val(0), next(nullptr) {}
+ *     ListNode(int x): val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next): val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode();
+        ListNode* current = dummy;
+        int carry = 0;
+
+        while (l1 || l2 || carry) {
+            int v1 = l1 ? l1->val : 0;
+            int v2 = l2 ? l2->val : 0;
+
+            int sum = v1 + v2 + carry;
+            carry = sum / 10;
+
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+
+        return dummy->next;
+    }
+};
